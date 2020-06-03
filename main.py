7252793a85003db2296d1a0396ff48ca7a077949
Philip <p208p2002@gmail.com>
2020-06-03 03:24:38 +0000

    no message

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import logging
-import SimpSOM as sps
 import numpy as np
+from som import SOM
+import logging
+
 
 FORMAT = '%(filename)s line:%(lineno)d\t%(message)s'
 logging.basicConfig(level=logging.INFO,format=FORMAT)
 print = logging.info
+
 
 def make_XY(data):
     X,Y = [],[]
@@ -13,36 +15,23 @@ def make_XY(data):
         X.append(d[1:])
         Y.append(d[0])
     #
-    Y = [int(y) for y in Y]
+    Y = [int(y)-1 for y in Y]
     X = np.array([np.array([float(f) for f in x]) for x in X])
                 
     return X,Y
     
+
 if __name__ == "__main__":
     with open('wine.data','r',encoding='utf-8') as f:
         data = f.read()
     data = data.split('\n')[:-1]
     X,Y = make_XY(data)
 
-    FEATURE_COUNT = len(X[0])
-    CLASS_COUNT = len(list(set(Y)))
-    print('feature_count:%d'%FEATURE_COUNT)
-    print('class_count:%d'%CLASS_COUNT)
+    som = SOM(8, 8)  # initialize the SOM
+    som.fit(X, 10000, save_e=True, interval=100)  # fit the SOM for 10000 epochs, save the error every 100 steps
+    som.plot_error_history(filename='images/som_error.png')  # plot the training error history
 
-    net = sps.somNet(20, 20, X, PBC=True)
-    #Train the network for 10000 epochs and with initial learning rate of 0.01. 
-    net.train(0.008, 15000)
-
-    #Save the weights to file
-    net.save('filename_weights')
-
-    #Print a map of the network nodes and colour them according to the first feature (column number 0) of the dataset
-    #and then according to the distance between each node and its neighbours.
-    net.nodes_graph(colnum=0)
-    net.diff_graph()
-
-    #Project the datapoints on the new 2D network map.
-    net.project(X, labels=Y)
-
-    #Cluster the datapoints according to the Quality Threshold algorithm.
-    net.cluster(X, type='qthresh')	
+    # now visualize the learned representation with the class labels
+    som.plot_point_map(X, Y, ['Class %d'%(l+1) for l in range(3)], filename='images/som.png')
+    som.plot_class_density(X, Y, t=0, name='Class 0', filename='images/class_0.png')
+    som.plot_distance_map(filename='images/distance_map.png')  # plot the distance map after training
